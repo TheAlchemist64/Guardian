426504e357f8c418d262bd76b29c3a896d2ab497
Daniel S <TheAlchemist64@noreply.github.com>
2017-07-28 23:01:25 -0500

    Draw a 9x9 grid onto the screen

--- main.lua
+++ main.lua
@@ -1,5 +1,14 @@
 function love.load()
-
+	--Initialize Map
+	MAP_SIZE = 9
+	TILE_SIZE = 16
+	map = {}
+	for x=1,MAP_SIZE do
+		map[x] = {}
+		for y=1,MAP_SIZE do
+			map[x][y] = {}
+		end
+	end
 end
 
 function love.update(dt)
@@ -7,5 +16,14 @@ function love.update(dt)
 end
 
 function love.draw()
-	
+	--Draw Map
+	for x, col in ipairs(map) do
+		for y, tile in ipairs(col) do
+			local dmns = {(x-1) * TILE_SIZE, (y-1) * TILE_SIZE, TILE_SIZE, TILE_SIZE}
+			love.graphics.setColor(0, 0, 255);
+			love.graphics.rectangle('fill', unpack(dmns))
+			love.graphics.setColor(255,255,255)
+			love.graphics.rectangle('line', unpack(dmns))
+		end
+	end
 end
